14db05ebef498e1e05aba2effa752e59abc7b73f
chomotngayloihuanohoa <101667420+chomotngayloihuanohoa@users.noreply.github.com>
2025-02-11 21:35:44 +0700

    Initialize Web Application

---
+++ src/main/java/dev/car/carsell/CarsellApplication.java
@@ -0,0 +1,13 @@
+package dev.car.carsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/car/carsell/CarsellApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.car.carsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
